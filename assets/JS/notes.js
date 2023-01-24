1eeb58bdcb2e1fb8b2f711a786f3d4a0cfbb7eab
allbutgold <contact@leokieffer.com>
2023-01-24 13:14:44 +0100

    String methods, more if else, switch

--- assets/JS/notes.js
+++ assets/JS/notes.js
@@ -2,7 +2,22 @@
 
 // JavaScript is case sensetiv!! 
 /* ============================================== */
-/* VARIABLE NAME CONVENTIONS:
+
+/* // ====== VARIABLES ======    "let","const" and "var" (use "const" by default)
+
+    "let": is used to declare a changable variable (ressasign, mutate value) */
+    let age = 30;
+    age = 31;
+    
+    //  "const": a variable declared with const cannot be changed (immutable variable). You cannot declare empty const values.
+    const birthyear = 1991;
+    
+    //  "var": the old (pre ES6) way to write variables. Do not use.    
+    var job = "programmer";
+    job = "teacher";
+    
+
+/* === VARIABLE NAME CONVENTIONS ===
 
 - first word starting small all follwing starting with capital letter
 - cannot start with numbers
@@ -31,10 +46,11 @@ console.log(_nope)
 // $ begins with $ sign
 let $auchNope = "auch nicht benutzen"
 console.log($auchNope)
-
 /* ============================================== */
 
-/* === DATA TYPES ===
+
+
+/* // ====== DATA TYPES ======
 
 Number(23): floating point number
     - used for decimals and integers */
@@ -63,25 +79,12 @@ console.log(typeof true);
 let javascriptIsFun = true;
 //  change value 
 javascriptIsFun = "YES!";
-
 /* ============================================== */
 
-/* === VARIABLES ===    "let","const" and "var" (use "const" by default)
 
-    "let": is used to declare a changable variable (ressasign, mutate value) */
-let age = 30;
-age = 31;
 
-//  "const": a variable declared with const cannot be changed (immutable variable). You cannot declare empty const values.
-const birthyear = 1991;
 
-//  "var": the old (pre ES6) way to write variables. Do not use.    
-var job = "programmer";
-job = "teacher";
-
-/* ============================================== */
-
-/* === BASIC OPERATORS === allows to transform or combine values (as well as do other things)
+/* ====== OPERATORS ====== allows to transform or combine values (as well as do other things)
 
 Basic math operators: you can do basic calculations with values and simple math operators */
 const now = 2050;
@@ -146,10 +149,69 @@ console.log(x, y);
 // You can execute operations first by putting then into paranthesese
 const averageAge = (ageLeo + ageJeni) / 2;
 console.log(ageLeo, ageJeni, averageAge);
-
 /* ============================================== */
 
+
+
+// ====== STRINGS ======
+// ==== String Methods ====
+const textOne = "Wo wohnt Stefan?";
+console.log(textOne);
+
+// == indexOF ==
+// string.indexOF positions Angabe start counting at 0 
+
+const wohntIndex = textOne.indexOf("wohnt");
+console.log(wohntIndex); // 3
+
+const stefanIndex = textOne.indexOf("Stefan");
+console.log(stefanIndex); // 9
+
+const result = textOne.indexOf("hallo")
+console.log(result); // -1 means text does not contain this string.
+
+// == includes ==
+// string.includes checks if a string contains another string 
+console.log(textOne.includes("Stefan")); // true
+
+// == slice ==
+// string.slice can cut a string and store it in a different one. The original string stays untouched.
+const spring = "Der Frühling ist nah!";
+
+console.log(spring.slice(0, 3)); // Cut the first 3 characters 
+console.log(spring.slice(4,12)); // cut the word Frühling
+
+console.log(spring);
+
+console.log(spring.slice(-1)); // Cuts the last character of a string
+console.log(spring.slice(0)); // Cuts the entire string
+
+// == replace ==
+// string.replace replaces first occurence of a string with a new one
+console.log(spring.replace("nah", "fern"));
+
+// string.replaceAll replaces all occurences of a string with a new one
+console.log(spring.replaceAll("nah", "fern"));
+
+// == toUpperCase ==
+// string.toUpperCase converts all letters to uppercase
+const lowText = "hier ist alles klein geschrieben.";
+console.log(lowText.toUpperCase());
+
+// == toLowerCase ==
+// string.toLowerCase converts all letters to lowercase
+const upText = "HieR IsT AlLEs DoOF.";
+console.log(upText.toLowerCase());
+console.log(upText); // original string will not be changed 
+
+// == length ==
+// string.length returns the length of a string
+const username = "SuperUser5000";
+console.log(username.length);
+
+
 /* === String Concatination === You can use the "+" operator to add strings to each other  */
+
 const firstName = "Karl Werner";
 const lastName = "Kieffer";
 console.log(firstName + lastName);
@@ -163,7 +225,7 @@ console.log("Mein MacBook hat " + (macBook * mwst) + " gekostet!" );
 // Use an empty string " " to add a space 
 console.log(firstName + " " + lastName);
 
-/* == STRINGS AND TEMPLATE LITERALS == : a template literal can assamble multiple pices into one final string
+/* === TEMPLATE LITERALS === : a template literal can assamble multiple pices into one final string
     to wirte a template string use backticks `` */
 
 const leo = "I'm " + firstName + ', a ' + (now - birthyear) + ' years old ' + job + '!';
@@ -192,26 +254,12 @@ const year = 2037;
 
 const leoBrandNew = `I'm ${myName},a ${year - yearOfBirth} year old ${occupation}!`;
 console.log(leoBrandNew);
-
 /* ============================================== */
 
-/* === TAKING DECISIONS === if / else statements: can take decsions using if else statements   */
 
-// == if/else control structure ==
-const Currentge = 19;
 
-if(age >= 18) { // condition: any code that returns a true or false value
-    console.log('Sarah can start driving license 🚗');}  // if condition is true, block will be executed
-        
-         else { // if condition is fals the else block will be executed (optional)
-            const yearsleft = 18 - Currentage;
-            console.log(`Sahra is to young. Wait another ${yearsleft} years :`);
-        }
-
-/* ============================================== */
-
-/* === TYPE CONVERSION AND TYPE COERCION === :  You can covert ?values? by using functions  like 'Number' or 'String. 
-    These have to start with a capiatal letter! You can only conver Numbers, Strings and Booleans. */
+/* === TYPE CONVERSION AND TYPE COERCION === :  You can covert values by using functions  like 'Number' or 'String. 
+    These have to start with a capiatal letter! You can only convert Numbers, Strings and Booleans. */
 
 // == type conversion ==
 const inputYear = '1991';
@@ -230,10 +278,11 @@ console.log('23' / "2");
 let n = '1' + 1;  // '11' (the number 1 in converted and added to the String '1')
 n=n - 1
 console.log(n)
-
 /* ============================================== */
 
-// === OUTPUT METHODS ===
+
+
+// ====== OUTPUT METHODS ======
 
 // document.write()
 document.write("Hallo zusammen") // use document.write to display something in browser 
@@ -242,8 +291,9 @@ document.write("<h2>Das ist schon besser!</h2>") // use document.write to displa
 // window.alert opens a popup when page is loaded to give an alert.
 /*     window.alert("Du hast Gewonnen!!!!!")
 */
-
 /* ============================================== */
+
+
 
 // === INPUT METHODS === 
 
@@ -256,9 +306,8 @@ console.log("Der Nuter ist " + howOld + " Jahre alt.") */
 /*     let confirm = window.confirm("Stimmen sie allem auf unserer Seite zu?")
 console.log(confirm) // true */
 
-/* ============================================== */
 
-// === Accessing HTML emelents through JS ===
+// ====== Accessing HTML emelents through JS ======
 
 // pulling element from HTML to JS and storing the ID headlineOne in a variable
 let headlineOne = document.getElementById("headlineOne"); 
@@ -289,10 +338,11 @@ let headlineFour = document.querySelector("h3:nth-of-type(3)")
 console.log(headlineFour);
 
 headlineFour.style.fontSize = "4rem"
-
 /* ============================================== */
 
-/* === FUNCTIONS === */
+
+
+/* ====== FUNCTIONS ====== */
 
 // store HTML element in variables. define variables at the start of the document
 let headline = document.getElementById("headline");
@@ -323,7 +373,7 @@ function changeColor() {
     container.style.backgroundColor = "blue"
 }
 
-/* == Acessing Data in a HTML form through a JS function == */
+/* === Acessing Data in a HTML form through a JS function === */
 
 // get Form Elements and store them in variables
 const aName = document.getElementById("aName");
@@ -362,8 +412,8 @@ function toggleStyles() {
 } */
 
 // short version
-button.addEventListener("click", function() {
-    text.classList.toggle("colorFontSize")
+button.addEventListener("click", function() {  // Add a click event to a <button> element element.addEventListener(event, function, useCapture)
+    text.classList.toggle("colorFontSize")  // on click of button toggle class colorFontSize from the element with id "text"
 })
 
 const darkMode = document.querySelector("#darkMode"); // define variable darkMode by selecting the element with id "darkMode"
@@ -371,3 +421,127 @@ const darkMode = document.querySelector("#darkMode"); // define variable darkMod
 darkMode.addEventListener("click", function() { // on click of button darkMode toggle CSS class .darkMode
     document.body.classList.toggle("darkMode");
 })
+/* ============================================== */
+
+
+
+/* ====== TAKING DECISIONS ======   */
+
+// === if/else control structure === if / else statements: can take decsions using if else statements 
+const currentAge = 19;
+
+if(age >= 18) { // condition: any code that returns a true or false value
+    console.log('Sarah can start driving license 🚗');}  // if condition is true, block will be executed
+        
+         else { // if condition is fals the else block will be executed (optional)
+            const yearsleft = 18 - currentAge;
+            console.log(`Sahra is to young. Wait another ${yearsleft} years :`);
+        }
+
+
+// if, else, else if
+const trainer = "Sergio";
+
+if (trainer === "Stefan") { // check this contidtion to see if the trainer is "Stefan"
+    console.log("Das ist ein Trainer");
+} else if (trainer === "Manuel") { // check this condtion to see if the trainer is "Manuel"
+    console.log("Das ist ein Trainer");
+} else { // if the trainer is not "Stefan" or "Manuel" log the message "Das ist dann wohl der Chef"
+    console.log("Das ist dann wohl der Chef");
+}
+
+
+// logical operators
+if (trainer === "Stefan" || trainer === "Manuel") {
+    console.log("Das sind unsere Trainer");
+} else {
+    console.log("Das ist ein anderer Mitarbeite");
+}
+
+// what season is it?
+const month = 12
+
+if (month === 4) {
+    console.log("Frühling");
+} else if (month === 8) {
+    console.log("Sommer");
+} else if (month === 10) {
+    console.log("Dezember");
+} else {
+    console.log("Winter");
+}
+
+if (month > 3 && month <= 6) {
+    console.log("Frühling");
+} else if (month > 6 && month <= 9) {
+    console.log("Sommer");
+} else if (month > 9 && month <= 12) {
+    console.log("Herbst");
+} else {
+    console.log("Winter");
+}
+
+// Alterspruefung and displaying the result on the page
+let alter = document.getElementById("#alterspruefung");
+const ageOutput = document.querySelector("#ageOutput");
+
+function ageCheck() {
+    const input = alter.value;
+    console.log(input);
+}
+
+
+if (input >= 18) {
+    ageOutput.innerHTML = "Du bist Volljährig!";
+} else if (input < 18 && input >= 0) {
+    ageOutput.innerHTML = "Du bist nicht Volljährig!";
+} else {
+    ageOutput.innerHTML = "Deine Angabe ist ungültig!";
+}
+
+function ageCheck(){
+    const input = alter.value
+    let output = " ";
+
+if (input >= 18) {  
+    output = "Du bist volljährig";
+} else if (input < 18 && input >= 0){
+    output = "Du bist nicht volljahrig";
+} else {
+    output = "Deine Eingabe ist unguültig";
+}}
+
+// === SWITCH Control Structure ===  Switch check if condition is true and then executes code block 
+
+    /* switch(expression) {
+        case x: 
+            code block to be executed
+            break
+        case y: 
+            code block to be executed
+        default:
+            code block to be executed */
+
+function getNameOfMonth() {
+    const input = Number(numberOfMonth.value);
+    console.log(input);
+    
+    let output = "";
+    
+    switch (input) { // check for the value of input
+        case 1:     // if input is 1
+            output = "Januar";
+            break
+        case 2:     // if input is 2 
+            output = "Februar";
+            break
+        case 3:     // if input is 3
+            output = "March";
+            break
+        default:    // everything else 
+            output = "and so on";
+    }
+    
+    document.getElementById("outputField").innerHTML = output
+    }
+    
